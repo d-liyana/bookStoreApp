ab5fa45c8bb9de9fa765e2766b539b420156786e
d-liyana <perera0011@gmail.com>
2021-04-24 09:43:26 +1000

    changes

--- storeapp/storeapp/settings.py
+++ storeapp/storeapp/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'storeapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'bookStore.sqlite3',
+        'NAME': BASE_DIR / 'bookstore.sqlite3',
     }
 }
 
